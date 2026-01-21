99afc42cac162c8c77cac584ea2ce0709564d0e7
Sachi <sachinthadilshan24@gmail.com>
2026-01-22 00:41:03 +0530

    Fix TrustProxies headers for Laravel 11

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,11 +9,15 @@ use Spatie\Permission\Middleware\RoleOrPermissionMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // Trust Render proxy (fixes HTTPS + CSRF + 419)
+        $middleware->append(\App\Http\Middleware\TrustProxies::class);
+
+        // Spatie permission aliases
         $middleware->alias([
             'role' => RoleMiddleware::class,
             'permission' => PermissionMiddleware::class,
